ddb3b4745394e530b7f5d631c49a3e0191ecc3ec
Jordy Murgueitio <jordinho.15@gmail.com>
2023-02-14 11:18:17 -0500

    adding js for form

--- index.js
+++ index.js
@@ -6,3 +6,15 @@
         x.style.display = "none";
         }
     }
+
+    function emailSend () {
+        Email.send({
+            SecureToken : "51744d62-7800-43f2-a396-5b48ec8bbb8d",
+            To : 'jordinho.15@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : "New message",
+            Body : document.getElementById("name") + "age" + document.getElementById("age") + document.getElementById("email")
+        }).then(
+            message => alert(message)
+        );
+    }
