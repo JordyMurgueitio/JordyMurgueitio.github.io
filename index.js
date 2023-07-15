72f8c447fbdded03ad43a827193589ca6c78ac52
Jordy Murgueitio <jordinho.15@gmail.com>
2023-07-15 10:15:48 -0500

    adding navbar style

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-    function myFunction() {
-        var x = document.getElementById("collapsibleNavbar");
-        if (x.style.display === "none") {
-        x.style.display = "block";
-        } else {
-        x.style.display = "none";
-        }
-    }
+// VARIABLES
 
-    function emailSend () {
-        Email.send({
-            SecureToken : "51744d62-7800-43f2-a396-5b48ec8bbb8d",
-            To : 'jordinho.15@gmail.com',
-            From : document.getElementById("email").value,
-            Subject : "New message",
-            Body : document.getElementById("name") + "age" + document.getElementById("age") + document.getElementById("email")
-        }).then(
-            message => alert(message)
-        );
+const navBar = document.getElementById("nav");
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        navBar.style.top = "0";
+        navBar.style.position = "fixed";
+        navBar.style.backgroundColor = "#131313"
+        navBar.style.borderBottom= "1px solid white"
+        navBar.style.zIndex = "2"
+    } else {
+        navBar.style.top = "0";
+        navBar.style.position = "absolute";
+        navBar.style.backgroundColor = "#13131300"
+        navBar.style.borderBottom= ""
     }
+}
+
+window.onscroll = function() {scrollFunction()};
+
+
