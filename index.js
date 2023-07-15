3516610e634ad32a59c046dec7e412e55c1b5925
Jordy Murgueitio <jordinho.15@gmail.com>
2023-07-15 10:44:27 -0500

    adding small improvements

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // VARIABLES
 
 const navBar = document.getElementById("nav");
+const header = document.getElementById("header");
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         navBar.style.top = "0";
         navBar.style.position = "fixed";
         navBar.style.backgroundColor = "#131313"
         navBar.style.borderBottom= "1px solid white"
         navBar.style.zIndex = "2"
+        header.style.marginTop = "60px";
     } else {
         navBar.style.top = "0";
         navBar.style.position = "absolute";
         navBar.style.backgroundColor = "#13131300"
-        navBar.style.borderBottom= ""
+        navBar.style.borderBottom= "";
+        header.style.marginTop = "0px";
     }
 }
 
